6a6493bb8fa87d559339ab7e9bc00024bdebccc7
Ext. Martinez <ext.joshua.martinez@mann-hummel.com>
2025-11-17 11:59:49 -0600

    added help/tour button pt 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- This import is needed
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss()
   ],
-  // This is required to help Vite's dev server find the driver.js module.
-  optimizeDeps: { include: ['driver.js'] },
+  // This helps Vite's dev server find the driver.js module during startup.
+  optimizeDeps: { include: ['driver.js'] }
 })
